c7b00d0668368a61c8352ea2feaff87b4096143e
serhat <serhat.karabiyik@outlook.fr>
2023-02-12 20:03:37 +0100

    Update 1.1

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
-import {BiBook , BiWallet, BiMessageSquareDetail } from 'react-icons/bi'
-import { useState } from 'react'
-
-
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiWallet, BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 
 export default function Nav() {
-
-  const [activeNav, setActiveNav] = useState('#')
-
+  const [activeNav, setActiveNav] = useState("#");
 
   return (
     <nav>
-      <a href="#" onClick={()=>setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
-      <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#competence" onClick={()=>setActiveNav('#competence')} className={activeNav === '#competence' ? 'active' : ''}><BiBook /></a>
-      <a href="#portfolio" onClick={()=>setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiWallet /></a>
-      <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#competence"
+        onClick={() => setActiveNav("#competence")}
+        className={activeNav === "#competence" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <BiWallet />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
     </nav>
-  )
+  );
 }
